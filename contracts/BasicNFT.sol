8b74cd4a375d0870ea2d68a47ead26371ea11958
ragz-cmd <anuragmokashi01@gmail.com>
2024-06-02 20:24:17 +0530

    deploymet script and basic nft test

---
+++ contracts/BasicNFT.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT 
+pragma solidity 0.8.25;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+contract BasicNFT is ERC721{
+    string constant public TOKEN_URI='ipfs://bafybeigm3yuykzio2alkm4xtevgn6xgl5zmbevjkvli5cdsnnkn7ktclv4/';
+    uint256 private s_counter;
+    constructor() ERC721("katze","CAR"){
+        s_counter=0;
+    }
+    function mintNFT() public returns(uint256){
+        _safeMint(msg.sender,s_counter);
+        s_counter = s_counter +1;
+        return s_counter;
+    }
+
+    function getTokenCounter() public view returns(uint256){
+        return s_counter;
+    }
+
+    function tokenURI(uint256/*token id*/) public pure override returns(string memory){
+        return TOKEN_URI;
+    }
+
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,33 +1,25 @@
-require("@nomiclabs/hardhat-waffle")
-require("@nomiclabs/hardhat-etherscan")
-require("hardhat-deploy")
-require("solidity-coverage")
-require("hardhat-gas-reporter")
-require("hardhat-contract-sizer")
-require("dotenv").config()
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require("hardhat-deploy");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("hardhat-contract-sizer");
+require("dotenv").config();
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-const SEPOLIA_RPC_URL =
-    process.env.SEPOLIA_RPC_URL 
-    process.env.POLYGON_MAINNET_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY 
-
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY 
-
-const REPORT_GAS = process.env.REPORT_GAS || false
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const REPORT_GAS = process.env.REPORT_GAS === 'true';
 
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
-            // // If you want to do some forking, uncomment this
-            // forking: {
-            //   url: MAINNET_RPC_URL
-            // }
             chainId: 31337,
         },
         localhost: {
@@ -36,27 +28,19 @@ module.exports = {
         sepolia: {
             url: SEPOLIA_RPC_URL,
             accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            //   accounts: {
-            //     mnemonic: MNEMONIC,
-            //   },
             saveDeployments: true,
             chainId: 11155111,
         }
-        
     },
     etherscan: {
-        // npx hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
-        apiKey: {
-            sepolia: ETHERSCAN_API_KEY,
-            
-        },
+        apiKey: ETHERSCAN_API_KEY,
     },
     gasReporter: {
         enabled: REPORT_GAS,
         currency: "USD",
-        outputFile: "gas-report.txt",
-        noColors: true,
-        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+        noColors: true, // Set to false for better readability on the console
+        gasPrice: 20, 
+        coinmarketcap: process.env.COINMARKETCAP_API_KEY,
     },
     contractSizer: {
         runOnCompile: false,
@@ -64,8 +48,7 @@ module.exports = {
     },
     namedAccounts: {
         deployer: {
-            default: 0, // here this will by default take the first account as deployer
-            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+            default: 0,
         },
         player: {
             default: 1,
@@ -84,4 +67,4 @@ module.exports = {
     mocha: {
         timeout: 200000, // 200 seconds max for running tests
     },
-}
+};
